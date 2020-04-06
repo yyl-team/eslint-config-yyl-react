2d6738b8eee31d889eb06df6b0cbc14199ec9ef8
liudaojie <liudaojie@yy.com>
2020-04-06 13:23:07 +0800

    feat: 更新test case

--- test/base/index.ts
+++
@@ -1,9 +0,0 @@
-const a = 1;
-(async () => {
-  const r = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(2)
-    }, 100)
-  })
-  console.log(r)
-})().catch(() => undefined)
