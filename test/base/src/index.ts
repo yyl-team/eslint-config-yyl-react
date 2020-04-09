b93ac429f7ec487cdc51a3b163aa98a7cc2b3fce
liudaojie <liudaojie@yy.com>
2020-04-09 11:42:04 +0800

    feat: 调整测试用例

--- test/base/src/index.ts
+++ test/base/src/index.ts
@@ -1,4 +1,9 @@
 const a = 1
+
+const obj = {
+  a: 'a',
+  b: 0
+}
 ;(async () => {
   const r = await new Promise(resolve => {
     setTimeout(() => {
@@ -6,4 +11,5 @@ const a = 1
     }, 100)
   })
   console.log(r)
+  console.log(obj)
 })().catch(() => undefined)
